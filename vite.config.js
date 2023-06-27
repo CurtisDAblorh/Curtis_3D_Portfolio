3e1c508af354b18a065f34f9e14f2ea97bc03ee0
CurtisDAblorh <126522677+CurtisDAblorh@users.noreply.github.com>
2023-06-27 11:37:56 +0100

    add:deploy fix

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: "/vite-deploy/"
+  base: "/Curtis_3D_Portfolio/"
 })
