3e4b6be6b828325cfaa7003ff6b3d3ad56646c66
CurtisDAblorh <126522677+CurtisDAblorh@users.noreply.github.com>
2023-07-03 10:05:46 +0100

    file size reduction

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,4 @@
 import logo from "./logo.svg";
-// import funkome from "./funkome.svg";
 import funkomepng from "./funkome.png";
 import backend from "./backend.png";
 import creator from "./creator.png";
@@ -11,30 +10,11 @@ import menu from "./menu.svg";
 import close from "./close.svg";
 import bright from "./company/bright.svg";
 
-import css from "./tech/css.png";
-import docker from "./tech/docker.png";
-import figma from "./tech/figma.png";
-import git from "./tech/git.png";
-import html from "./tech/html.png";
-import javascript from "./tech/javascript.png";
-import mongodb from "./tech/mongodb.png";
-import nodejs from "./tech/nodejs.png";
-import reactjs from "./tech/reactjs.png";
-import redux from "./tech/redux.png";
-import tailwind from "./tech/tailwind.png";
-import typescript from "./tech/typescript.png";
-import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
 import wanstor from "./company/wanstor.png";
 import wagamama from "./company/wagamama.png";
 import fedex from "./company/fedex.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 import ecommerce from "./ecommerce.png";
 import portfolio2 from "./portfolio2.png";
 import calc from "./calc.png";
@@ -53,7 +33,6 @@ portfolio2,
   bright,
   wagamama,
   wanstor,
-  // funkome,
   funkomepng,
   logo,
   backend,
@@ -64,23 +43,5 @@ portfolio2,
   webpage,
   menu,
   close,
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  mongodb,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  threejs,
-  meta,
-  shopify,
-  starbucks,
-  carrent,
-  jobit,
-  tripguide,
+ 
 };
